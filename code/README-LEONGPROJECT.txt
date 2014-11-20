ab80840ffbee99db8abb9d83507f9199fbe89dfd
DanielLeong <daniel.leong@students.olin.edu>
2014-11-19 23:40:00 -0500

    Final Commit. See readme for info about project

---
+++ code/README-LEONGPROJECT.txt
@@ -0,0 +1,9 @@
+This folder contains code for the final project of Daniel Leong
+11/19/2014
+BaseballHomers.py is the python script for the project. 
+LeongWordReport.docx is the word copy of the report
+LeongWordReport.pdf is the final pdf
+
+Happy Grading and thank you for teaching!!!!
+Best,
+Daniel
--- code/baseballhomers.py
+++ code/baseballhomers.py
@@ -3,6 +3,9 @@ by Allen B. Downey, available from greenteapress.com
 
 Copyright 2014 Allen B. Downey
 License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
+
+The Robinson Cano Problem
+This code seeks to calculate rest of season Home runs based on career and season totals
 """
 
 from __future__ import print_function, division
@@ -33,52 +36,61 @@ class Season(thinkbayes2.Suite):
         gameFraction = rem_games/162
         for lam, prob in self.Items():
             lt = lam*gameFraction
-            pmf = thinkbayes2.MakePoissonPmf(lt, 60)
-            # thinkplot.Pdf(pmf, linewidth=1, alpha=.2, color='purple')
+            pmf = thinkbayes2.MakePoissonPmf(lt, int(homerunAvg*2))
             metaPmf[pmf] = prob
         superMeta = thinkbayes2.MakeMixture(metaPmf)
         superMeta += score
-        print(superMeta.ProbGreater(homerunAvg))
-        print(superMeta.MaximumLikelihood())
-        # thinkplot.Pdf(superMeta)
+        totalHR = superMeta.MaximumLikelihood()
+        thinkplot.Pdf(superMeta, label="Predictive Distribution")
+        return totalHR
 
 def homersSeasonCalculator(homerunHistory, gamesPlayed, currentHR):
-    diffList = []
+    """" Given previous four years, number of games played in current season,
+        and current HR total will calculate the rate of home runs for rest of 
+        year"""
+
     weightedDelta = 0
+
     for i in range(len(homerunHistory)-1):
         delta = homerunHistory[i+1]-homerunHistory[i]
         weightedDelta += float(delta)*(i+1)/6
 
-    print(weightedDelta)
     previousYearHR = homerunHistory[-1]
     homersSeasonBase = previousYearHR + weightedDelta
     seasonFraction = float(gamesPlayed)/162
     homersSeasonRate = currentHR / seasonFraction
-    print(homersSeasonRate)
+
     compositeHomers = seasonFraction*homersSeasonRate + (1-seasonFraction) * homersSeasonBase
 
     return compositeHomers
 
 def main():
+    """ Main loop creates a vector, updates it with home run totals and predicts
+        number of home runs that Hitter will hit during season.
+        previousYears = Cano's previous four home run totals
+        gamesRemaining = Number of games left in 162 Game seasonFraction
+        homeRuns: Home runs at point in season evaluated"""
+
     previousYears = [29, 28, 33, 27]
     gamesRemaining = 95
     homeRuns = 2
+
     homersSeason = homersSeasonCalculator(previousYears, (162-gamesRemaining), homeRuns)
     print(homersSeason)
     previousYears.append(homersSeason)
-    hypos = numpy.linspace(0, homersSeason*2, 201)
+    hypos = numpy.linspace(0, homersSeason*2.25, 201)
     suite = Season(hypos)
     
     for year in previousYears:
         suite.Update(162/year)
     
-    thinkplot.Pdf(suite, label='prior')
+    # thinkplot.Pdf(suite, label='prior')
 
-    suite.PredRemaining(gamesRemaining,homeRuns,homersSeason)
+    totalHR = suite.PredRemaining(gamesRemaining,homeRuns,homersSeason)
+    print('Robinson Cano HR total 2014: ' + str(totalHR))
 
-
-    thinkplot.Show()
-
+    # thinkplot.Show(xlabel="Home Runs", ylabel="Pmf", title='Prior Distribtion of HRs')
+    thinkplot.Save(root = 'BaseballPosterior', xlabel="Home Runs", ylabel="Pmf", title='Distribution of Cano 2014 Home Run Total', formats=['png'])
 
 if __name__ == '__main__':
     main()
